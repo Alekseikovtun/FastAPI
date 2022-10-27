530c9bcb4c066cbe6c3f0f30ce94c71b5ead143a
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-10-27 20:39:02 +0300

    Testing FastAPI

---
+++ schemas.py
@@ -0,0 +1,41 @@
+from typing import List
+from pydantic import BaseModel, validator, Field
+from datetime import date
+
+
+class Genre(BaseModel):
+    name: str
+
+
+class Author(BaseModel):
+    first_name: str = Field(
+        ...,
+        max_length=25
+    )
+    last_name: str
+    age: int = Field(
+        ...,
+        gt=15,
+        lt=90,
+        description='Author age must be > 15 and < 90'
+    )
+
+    # @validator('age')
+    # def check_age(cls, v):
+    #     if v < 15 > 90:
+    #         raise ValueError('Author age must be > 15')
+    #     return v
+
+
+class Book(BaseModel):
+    title: str
+    writer: str
+    duration: str
+    date: date
+    summory: str
+    genres: List[Genre] = []
+    pages: int = 10
+
+
+class BookOut(Book):
+    id: int
